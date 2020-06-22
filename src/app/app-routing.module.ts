e9117c43f6b5ab82262fe40ee5acdae23065f2a1
maureenchepkirui <maureenchepkirui005@gmail.com>
2020-06-22 09:34:10 +0300

    Create routes which will tell the router which view to display when a user clicks or pastes a url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ProfileComponent } from './profile/profile.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profile', component: ProfileComponent},
+  { path: 'about', component: AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
